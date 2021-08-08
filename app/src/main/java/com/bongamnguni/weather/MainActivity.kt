c9eb6c3c223e5948bcde1ddfc8f927d2c89a0864
Bonga Mnguni <bonga.kmnguni@gmail.com>
2021-08-08 20:12:17 +0200

    setting up database and adaptors

--- app/src/main/java/com/bongamnguni/weather/MainActivity.kt
+++ app/src/main/java/com/bongamnguni/weather/MainActivity.kt
@@ -2,11 +2,10 @@ package com.bongamnguni.weather
 
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import androidx.navigation.findNavController
+
 
 
 class MainActivity : AppCompatActivity() {
---
+++ app/src/main/java/com/bongamnguni/weather/adaptors/FavoriteAdaptor.kt
@@ -0,0 +1,49 @@
+package com.bongamnguni.weather.adaptors
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bongamnguni.weather.R
+import com.bongamnguni.weather.database.FavoriteModel
+
+
+class FavoriteAdaptor(private val items: MutableList<FavoriteModel>) : RecyclerView.Adapter<FavoriteAdaptor.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.favorite_row, parent, false)
+        return ViewHolder(view)
+    }
+
+    fun setListData(data: List<FavoriteModel>) {
+        this.items.addAll(data)
+        notifyDataSetChanged()
+    }
+
+    fun clearListData() {
+        this.items.clear()
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount() = items.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        holder.bind(items[position])
+
+    }
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val textViewTemp: TextView = itemView.findViewById(R.id.textViewTemperature)
+        val textViewPlace: TextView = itemView.findViewById(R.id.textViewPlace)
+
+
+        fun bind(data: FavoriteModel) {
+
+            textViewPlace.text =("${data.place}")
+            val roundTemp = Math.round(data.temperature)
+            textViewTemp.text = "$roundTemp °"
+        }
+    }
+}
